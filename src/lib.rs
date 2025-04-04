1c1dc3435f9b7654fda9d68ef547c7b7276a8b43
Mike Dilger <mike@mikedilger.com>
2025-04-04 15:10:03 +1300

    lib.rs file comment, warning settings

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// Copyright 2025 mosaic-store-lmdb Developers
+// Licensed under the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>
+// This file may not be copied, modified, or distributed except according to those terms.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+//! Defines a Store type for storing, indexing, and accessing mosaic records.
+//!
+//! Indexes point at offsets into a record map to avoid additional tree searches during lookups.
+//!
+//! Tries to comply with as many mosaic spec requirements as possible that can be fully
+//! implemented at the storage layer.
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#![deny(
+    missing_debug_implementations,
+    trivial_numeric_casts,
+    clippy::string_slice,
+    unused_import_braces,
+    unused_results,
+    unused_lifetimes,
+    unused_labels,
+    unused_extern_crates,
+    non_ascii_idents,
+    keyword_idents,
+    deprecated_in_future,
+    unstable_features,
+    single_use_lifetimes,
+    unreachable_pub,
+    missing_copy_implementations,
+    missing_docs
+)]
